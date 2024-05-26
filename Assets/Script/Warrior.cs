6aab37642c8279a5ff8b2b137f2d19627d094aa3
Thinh Tran <tio212@gmail.com>
2024-05-27 00:24:57 +0300

    feat: init script

---
+++ Assets/Script/Warrior.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warrior : Player
+{
+
+    public Warrior(int health, int power, string name)
+    {
+        Health = health;
+        Power = power;
+        Name = name;
+    }
+
+    public override void Attack() {
+        Debug.Log("Warrior is attacking with AXE");
+    }
+
+
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
